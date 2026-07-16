8332eefe05b41afc4107a7f1faa8cb7909e748d1
blazerlion232 <telfordjoseph34@gmail.com>
2026-07-16 21:47:45 +0100

    Adding dividend plotting

--- Stock-Analytics-Dash/main.py
+++ Stock-Analytics-Dash/main.py
@@ -1,6 +1,8 @@
 from src.fetch import get_stock_data
 from src.plotting import plot_close_price
+from src.plotting import plot_dividends
 from src.info import display_stock_info
+
 
 def main():
     symbol = input("Enter stock ticker: ").upper()
@@ -8,6 +10,6 @@ def main():
     print(df.head())
     plot_close_price(df, symbol)
     display_stock_info(df)
-
+    plot_dividends(df,symbol)
 if __name__ == "__main__":
     main()
--- Stock-Analytics-Dash/src/plotting.py
+++ Stock-Analytics-Dash/src/plotting.py
@@ -1,10 +1,24 @@
 import matplotlib.pyplot as plt
 
 def plot_close_price(df, symbol):
-    plt.figure(figsize=(12,6))
+    plt.figure(figsize = (12,6))
     plt.plot(df.index,df["Close"])
     plt.title(f"{symbol} Closing Price")
     plt.xlabel("Date")
     plt.ylabel("Price ($)")
     plt.grid()
+    plt.show()
+
+def plot_dividends(stock, symbol):
+    dividend = stock.dividends
+    if dividend.empty:
+        print("No dividend information available.")
+        return
+    
+    plt.figure(figsize = (10,5))
+    plt.bar(dividend.index, dividend.value)
+    plt.title(f"{symbol} Dividend History")
+    plt.xlabel("Date")
+    plt.ylabel("Dividend ($)")
+    plt.grid(True)
     plt.show()
