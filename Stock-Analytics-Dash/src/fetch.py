93ce94d7b606754a97f3186bd172350e7074d7ab
blazerlion232 <telfordjoseph34@gmail.com>
2026-07-16 20:37:02 +0100

    making the ticker object reuasble

--- Stock-Analytics-Dash/src/fetch.py
+++ Stock-Analytics-Dash/src/fetch.py
@@ -1,10 +1,10 @@
 import yfinance as yf
 import pandas as pd
 
+def get_stock(symbol: str):
+    """ Return a yfinance ticker object"""
+    return yf.Ticker(symbol)
+
 def get_stock_data(symbol: str, period: str = "1mo" ) -> pd.DataFrame:
-    """
-    Download stock data from Yahoo finance
-    """
-    stock = yf.Ticker(symbol)
-    df = stock.history(period=period)
-    return df
+    stock = get_stock(symbol)
+    return stock.history(period=period)
