7dbe78466a8f3c22efba0df56d243d69d67c4910
blazerlion232 <telfordjoseph34@gmail.com>
2026-07-16 22:01:58 +0100

    adding splits and tidying up

--- Stock-Analytics-Dash/main.py
+++ Stock-Analytics-Dash/main.py
@@ -1,15 +1,20 @@
-from src.fetch import get_stock_data
-from src.plotting import plot_close_price
-from src.plotting import plot_dividends
+from src.fetch import get_stock_data, get_stock
+from src.plotting import plot_close_price, plot_dividends, plot_stock_splits
 from src.info import display_stock_info
 
 
 def main():
     symbol = input("Enter stock ticker: ").upper()
+
     df = get_stock_data(symbol)
+    stock = get_stock(symbol)
+
     print(df.head())
     plot_close_price(df, symbol)
-    display_stock_info(df)
-    plot_dividends(df,symbol)
+    
+    display_stock_info(stock)
+    plot_dividends(stock,symbol)
+    plot_stock_splits(stock,symbol)
+
 if __name__ == "__main__":
     main()
--- Stock-Analytics-Dash/src/plotting.py
+++ Stock-Analytics-Dash/src/plotting.py
@@ -16,9 +16,23 @@ def plot_dividends(stock, symbol):
         return
     
     plt.figure(figsize = (10,5))
-    plt.bar(dividend.index, dividend.value)
+    plt.bar(dividend.index, dividend.values)
     plt.title(f"{symbol} Dividend History")
     plt.xlabel("Date")
     plt.ylabel("Dividend ($)")
+    plt.grid(True)
+    plt.show()
+
+def plot_stock_splits(stock, symbol):
+    splits = stock.splits
+    if splits.empty:
+        print("No stock splits history available")
+        return
+    
+    plt.figure(figsize = (10,5))
+    plt.stem(splits.index, splits.values)
+    plt.title(f"{symbol} Stock Splits")
+    plt.xlabel("Dates")
+    plt.ylabel("Split Ratio")
     plt.grid(True)
     plt.show()
